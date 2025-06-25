ad2bc8d19e6d992da9f33eb89ff232c1be8543cb
Arsieruuu <rayhanallysha20@email.com>
2025-06-25 14:43:21 +0700

    update-profile

--- app/src/main/java/com/example/careerpath/fragments/HomeFragment.java
+++ app/src/main/java/com/example/careerpath/fragments/HomeFragment.java
@@ -1,19 +1,31 @@
 package com.example.careerpath.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.careerpath.ProfileActivity;
 import com.example.careerpath.R;
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Tambahkan kode di bawah ini untuk handle klik pada profile_image
+        ImageView profileImage = view.findViewById(R.id.profile_image);
+        profileImage.setOnClickListener(v -> {
+            Intent intent = new Intent(requireContext(), ProfileActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 }
